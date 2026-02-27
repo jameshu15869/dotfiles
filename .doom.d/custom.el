8dc191c09982215586cd01851da18da59d93e0d7
James Hu <jhudson15869@gmail.com>
2026-02-26 20:24:18 -0500

    doom
    doom emacs config

---
+++ .doom.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(avy-timeout-seconds 0.2)
+ '(global-subword-mode t)
+ '(global-superword-mode nil)
+ '(package-selected-packages '(eglot))
+ '(treemacs-position 'right)
+ '(which-key-idle-delay 0.1))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
