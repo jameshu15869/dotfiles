d4aeb9b0ec282e0dccbf692b05fb1a7882745406
James Hu <jhudson15869@gmail.com>
2026-04-28 13:38:51 -0400

    [fish] setup bob nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
+fish_add_path ~/.local/bin
+fish_add_path ~/.local/share/bob/nvim-bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
