b06484543af3925efc68f65fca49e2c38b8976d7
James Hu <jhudson15869@gmail.com>
2026-04-28 21:49:45 -0400

    [nvim] nvim install
    Saw some problems when moving to 0.12 with / searching in VSCodeNeovim,
    let's stay on 0.11 for now

--- .config/nvim/lua/config/vscode.lua
+++ .config/nvim/lua/config/vscode.lua
@@ -99,7 +99,7 @@ require("lazy").setup({
       version = false,
     },
     {
-      "ggandor/leap.nvim",
+      url = "https://codeberg.org/andyg/leap.nvim",
       config = function()
         vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
         vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
