1331b6eed583a804aca4042c020017b6625a71fd
James Hu <jhudson15869@gmail.com>
2026-04-29 20:57:46 -0400

    [fish][vscode] fzf integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,5 @@ fish_add_path ~/.local/share/bob/nvim-bin
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
+    fzf --fish | source
 end
