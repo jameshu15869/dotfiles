0f3bf5af392903e9ed5e797e70383bce8429cb37
James Hu <jhudson15869@gmail.com>
2026-05-02 19:35:57 -0400

    [fish] set $EDITOR to vim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/share/bob/nvim-bin
+set -gx EDITOR /usr/bin/vim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
