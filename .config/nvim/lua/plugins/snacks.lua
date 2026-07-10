2bf1955ce4873599f30231969c04260893f49915
James Hu <jhudson15869@gmail.com>
2026-07-10 23:46:11 +0800

    [nvim] small updates
    - disable relative line numbers
    - move snacks picker to the right

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            auto_close = true,
+            layout = {
+              layout = {
+                position = "right",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
